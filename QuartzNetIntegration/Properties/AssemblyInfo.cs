240a4920293e8f8ee33249b239e7c9bb1f448f3d
mausch <mauricioscheffer@gmail.com>
2013-04-07 18:05:13 -0300

    Version 0.3.0

--- QuartzNetIntegration/Properties/AssemblyInfo.cs
+++ QuartzNetIntegration/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Castle.Facilities.QuartzIntegration")]
-[assembly: AssemblyCopyright("Copyright © Mauricio Scheffer 2009-2012")]
+[assembly: AssemblyCopyright("Copyright © Mauricio Scheffer 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 [assembly: InternalsVisibleTo("QuartzNetIntegration.Tests")]
