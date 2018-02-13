33f9e7fbda3a8b5302afa83d1c923e7e7d768b44
Kristof De Causemaeker <kristof@decausemaeker.be>
2018-02-13 20:46:01 +0100

    Merging master branch into forked branch

---
+++ QuartzNetIntegration.TestsOld/FacilityTests.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Linq;
+using Castle.Core.Configuration;
+using Castle.MicroKernel.Facilities;
+using Castle.MicroKernel.Handlers;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+using Quartz;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Castle.Facilities.QuartzIntegration.Tests {
+    [TestFixture]
+    public class FacilityTests
+    {
+        private class DisposableJob : IJob, IDisposable
+        {
+            public static bool Disposed;
+
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose");
+                Disposed = true;
+            }
+
+            public async Task Execute(IJobExecutionContext context)
+            {
+                await Task.Run(() => Console.WriteLine("Execute"));
+            }
+        }
+
+        [Test]
+        public void Basic()
+        {
+            using (var c = new WindsorContainer())
+            {
+                var facilityConfig = new MutableConfiguration("facility");
+                var quartzConfig = facilityConfig.CreateChild("quartz")
+                    .Attribute("id", "quartznet");
+
+                quartzConfig.CreateChild("item", "qwe").Attribute("key", "qq");
+
+                c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", facilityConfig);
+
+                c.AddFacility("quartz", new QuartzFacility());
+
+                c.Resolve<IJobScheduler>();
+                c.Resolve<IScheduler>();
+            }
+        }
+
+        [Test]
+        public void DisposableJobIsDisposed()
+        {
+            using (var c = new WindsorContainer())
+            {
+                c.Register(Component.For<DisposableJob>().LifeStyle.Transient);
+                var config = new MutableConfiguration("facility");
+                config.CreateChild("quartz");
+                c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", config);
+
+                c.AddFacility("quartz", new QuartzFacility());
+
+                var scheduler = c.Resolve<IScheduler>();
+                var jobDetail = JobBuilder.Create<DisposableJob>().WithIdentity("somejob").Build();
+                var trigger = TriggerBuilder.Create().WithIdentity("sometrigger").WithSimpleSchedule(s => s.WithIntervalInSeconds(1)).Build();
+
+                var task = scheduler.ScheduleJob(jobDetail, trigger);
+                task.Wait();
+                var dateTimeOffset = task.Result;
+
+                Assert.IsFalse(DisposableJob.Disposed);
+                scheduler.Start();
+                Thread.Sleep(1000);
+                Assert.IsTrue(DisposableJob.Disposed);
+            }
+        }
+
+        [Test]
+        public void FacilityRegistersReleasingJobListener()
+        {
+            using (var c = new WindsorContainer())
+            {
+                var config = new MutableConfiguration("facility");
+                config.CreateChild("quartz");
+                c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", config);
+                c.AddFacility("quartz", new QuartzFacility());
+                var scheduler = c.Resolve<IScheduler>();
+                //Assert.IsNotNull(scheduler.GlobalJobListeners);
+                //Assert.AreEqual(2, scheduler.GlobalJobListeners.Count);
+
+                Assert.IsAssignableFrom(typeof(ReleasingJobListener),
+                    scheduler.ListenerManager.GetJobListeners().ToArray()[0]);
+            }
+        }
+
+        [Test]
+        public void GlobalJobListeners()
+        {
+            using (var c = new WindsorContainer())
+            {
+                var config = new MutableConfiguration("facility");
+                config.CreateChild("quartz").CreateChild("item", "qwe").Attribute("key", "qq");
+                config.CreateChild("globalJobListeners").CreateChild("item", "${jobli}");
+                c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", config);
+                c.Register(Component.For<IJobListener>().ImplementedBy<SomeJobListener>().Named("jobli"));
+                c.AddFacility("quartz", new QuartzFacility());
+                var scheduler = (QuartzNetScheduler)c.Resolve<IScheduler>();
+                foreach (var l in scheduler.ListenerManager.GetJobListeners())
+                    Console.WriteLine(l.Name);
+                Assert.AreEqual(2, scheduler.ListenerManager.GetJobListeners().Count);
+            }
+        }
+
+        [Test]
+        public void GlobalJobListeners_with_no_registration_throws()
+        {
+            Assert.Throws(typeof(HandlerException), () =>
+            {
+                using (var c = new WindsorContainer())
+                {
+                    var config = new MutableConfiguration("facility");
+                    config.CreateChild("quartz").CreateChild("item", "qwe").Attribute("key", "qq");
+                    config.CreateChild("globalJobListeners").CreateChild("item", "${jobli}");
+                    c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", config);
+                    c.AddFacility("quartz", new QuartzFacility());
+                    c.Resolve<IScheduler>();
+                }
+            });
+        }
+
+        [Test]
+        public void GlobalTriggerListeners()
+        {
+            using (var c = new WindsorContainer())
+            {
+                var config = new MutableConfiguration("facility");
+                config.CreateChild("quartz").CreateChild("item", "qwe").Attribute("key", "qq");
+                config.CreateChild("globalTriggerListeners").CreateChild("item", "${jobli}");
+                c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", config);
+                c.Register(Component.For<ITriggerListener>().ImplementedBy<SomeTriggerListener>().Named("jobli"));
+                c.AddFacility("quartz", new QuartzFacility());
+
+                var scheduler = (QuartzNetScheduler)c.Resolve<IScheduler>();
+                foreach (var l in scheduler.ListenerManager.GetTriggerListeners())
+                    Console.WriteLine(l.Name);
+                Assert.AreEqual(1, scheduler.ListenerManager.GetTriggerListeners().Count);
+            }
+        }
+
+        [Test]
+        public void JobListeners()
+        {
+            using (var c = new WindsorContainer())
+            {
+                var config = new MutableConfiguration("facility");
+                config.CreateChild("quartz").CreateChild("item", "qwe").Attribute("key", "qq");
+                var listenerConfig = config.CreateChild("jobListeners");
+                listenerConfig
+                    .CreateChild("job")
+                    .Attribute("name", "someJob")
+                    .CreateChild("listener", "${jobli}");
+                c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", config);
+
+                c.Register(Component.For<IJobListener>().ImplementedBy<SomeJobListener>().Named("jobli"));
+                c.AddFacility("quartz", new QuartzFacility());
+
+                var scheduler = (QuartzNetScheduler)c.Resolve<IScheduler>();
+                foreach (var l in scheduler.ListenerManager.GetJobListeners())
+                    Console.WriteLine(l);
+                var jobli = scheduler.ListenerManager.GetJobListener(typeof(SomeJobListener).AssemblyQualifiedName);
+                Assert.IsNotNull(jobli);
+            }
+        }
+
+        [Test]
+
+        public void NoConfig_throws()
+        {
+            Assert.Throws(typeof(FacilityException), () =>
+            {
+                using (var c = new WindsorContainer())
+                {
+                    c.AddFacility("quartz", new QuartzFacility());
+                }
+            });
+        }
+
+        [Test]
+        public void NoProps_throws()
+        {
+            Assert.Throws(typeof(FacilityException), () =>
+            {
+                using (var c = new WindsorContainer())
+                {
+                    c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz",
+                        new MutableConfiguration("facility"));
+                    c.AddFacility("quartz", new QuartzFacility());
+                }
+            });
+        }
+
+        [Test]
+        public void TriggerListeners()
+        {
+            using (var c = new WindsorContainer())
+            {
+                var config = new MutableConfiguration("facility");
+                config.CreateChild("quartz").CreateChild("item", "qwe").Attribute("key", "qq");
+                var listenerConfig = config.CreateChild("triggerListeners");
+                listenerConfig
+                    .CreateChild("job")
+                    .Attribute("name", "someJob")
+                    .CreateChild("listener", "${trigli}");
+                c.Kernel.ConfigurationStore.AddFacilityConfiguration("quartz", config);
+
+                c.Register(Component.For<ITriggerListener>().ImplementedBy<SomeTriggerListener>().Named("trigli"));
+                c.AddFacility("quartz", new QuartzFacility());
+
+                var scheduler = (QuartzNetScheduler)c.Resolve<IScheduler>();
+                foreach (var l in scheduler.ListenerManager.GetTriggerListeners())
+                    Console.WriteLine(l);
+                var trigli =
+                    scheduler.ListenerManager.GetTriggerListener(typeof(SomeTriggerListener).AssemblyQualifiedName);
+                Assert.IsNotNull(trigli);
+            }
+        }
+    }
+}
---
+++ SampleAppOld/SampleSchedulerListener.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace SampleApp
+{
+    public class SampleSchedulerListener : ISchedulerListener
+    {
+        public async Task JobScheduled(ITrigger trigger, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobScheduled", token);
+        }
+
+        public async Task JobUnscheduled(TriggerKey triggerKey, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobUnscheduled", token);
+        }
+
+        public async Task TriggerFinalized(ITrigger trigger, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".TriggerFinalized", token);
+        }
+
+        public async Task TriggerPaused(TriggerKey triggerKey, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".TriggerPaused", token);
+        }
+
+        public async Task TriggersPaused(string triggerGroup, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".TriggersPaused", token);
+        }
+
+        public async Task TriggerResumed(TriggerKey triggerKey, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".TriggerResumed", token);
+        }
+
+        public async Task TriggersResumed(string triggerGroup, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".TriggersResumed", token);
+        }
+
+        public async Task JobAdded(IJobDetail jobDetail, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobAdded", token);
+        }
+
+        public async Task JobDeleted(JobKey jobKey, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobDeleted", token);
+        }
+
+        public async Task JobPaused(JobKey jobKey, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobPaused", token);
+        }
+
+        public async Task JobInterrupted(JobKey jobKey, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobInterupted", token);
+        }
+
+        public async Task JobsPaused(string jobGroup, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobsPaused", token);
+        }
+
+        public async Task JobResumed(JobKey jobKey, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobResumed", token);
+        }
+
+        public async Task JobsResumed(string jobGroup, CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".JobsResumed", token);
+        }
+
+        public async Task SchedulerError(string msg, SchedulerException cause,
+            CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".SchedulerError", token);
+        }
+
+        public async Task SchedulerInStandbyMode(CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".SchedulerInStandbyMode", token);
+        }
+
+        public async Task SchedulerStarted(CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".SchedulerStarted", token);
+        }
+
+        public async Task SchedulerStarting(CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".SchedulerStarting", token);
+        }
+
+        public async Task SchedulerShutdown(CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".SchedulerShutdown", token);
+        }
+
+        public async Task SchedulerShuttingdown(CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".SchedulerShuttingdown", token);
+        }
+
+        public async Task SchedulingDataCleared(CancellationToken token = default(CancellationToken))
+        {
+            await WriteMesssage(".SchedulingDataCleared", token);
+        }
+
+        private Task WriteMesssage(string message, CancellationToken token = default(CancellationToken))
+        {
+            return Task.Run(() => Console.WriteLine("{0}.{1}", GetType().Name, message), token);
+        }
+    }
+}
